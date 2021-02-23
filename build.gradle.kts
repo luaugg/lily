43f66fd374158ce6ea265bf39ac2e5da62118701
Sam Pritchard <sampritchard0801@gmail.com>
2021-02-23 17:12:31 +0000

    basic setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,12 @@ plugins {
 
 repositories {
     jcenter()
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
-
+    implementation("com.mewna:catnip:8a3ffef")
+    implementation("io.github.classgraph:classgraph:4.8.102")
 }
 
 application {
--- src/main/java/io/github/samophis/lily/startup/Main.java
+++ src/main/java/io/github/samophis/lily/startup/Main.java
@@ -1,2 +1,16 @@
-package io.github.samophis.lily.startup;public class Main {
+package io.github.samophis.lily.startup;
+
+import com.mewna.catnip.Catnip;
+import com.mewna.catnip.CatnipOptions;
+import com.mewna.catnip.shard.GatewayIntent;
+
+public class Main {
+    public static void main(String... args) {
+        final var token = System.getenv("LILY_BOT_TOKEN");
+        final var catnipOptions = new CatnipOptions(token);
+        catnipOptions.intents(GatewayIntent.UNPRIVILEGED_INTENTS);
+
+        final var catnip = Catnip.catnip(catnipOptions);
+        catnip.connect();
+    }
 }
