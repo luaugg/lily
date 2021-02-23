6a56ba7817b3ffd63f21f4f5031893f89f0cc59a
Sam Pritchard <sampritchard0801@gmail.com>
2021-02-23 17:12:19 +0000

    command annotation

--- src/main/java/io/github/samophis/lily/commands/Command.java
+++ src/main/java/io/github/samophis/lily/commands/Command.java
@@ -1,2 +1,26 @@
-package io.github.samophis.lily.commands;public interface Command {
+package io.github.samophis.lily.commands;
+
+
+import com.mewna.catnip.entity.util.Permission;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@SuppressWarnings("unused")
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Command {
+    // TODO: command categories
+
+    String[] value(); // names/aliases
+
+    String description() default "No available description.";
+
+    String usage() default "No available usage.";
+
+    Permission[] botPermissions() default { };
+
+    Permission[] userPermissions() default { };
 }
