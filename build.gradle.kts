7189e37ee090a969f0963438a4d19f40abf5703a
Sam Pritchard <sampritchard0801@gmail.com>
2021-05-14 04:28:56 +0100

    add Jedis as a dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 dependencies {
     implementation("net.dv8tion:JDA:4.2.1_262")
     implementation("io.sentry:sentry-logback:5.0.0-beta.3")
+    implementation("redis.clients:jedis:3.6.0")
 }
 
 application {
