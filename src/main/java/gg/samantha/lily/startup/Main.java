d85b9b60a860ec4c225c50a193a122c6b8bd328d
Sam Pritchard <sampritchard0801@gmail.com>
2021-05-14 03:36:53 +0100

    Remove unnecessary import.

--- src/main/java/gg/samantha/lily/startup/Main.java
+++ src/main/java/gg/samantha/lily/startup/Main.java
@@ -1,6 +1,5 @@
 package gg.samantha.lily.startup;
 
-import io.sentry.Sentry;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -12,7 +11,7 @@ import javax.security.auth.login.LoginException;
 
 public class Main extends ListenerAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-    private static final Main INSTANCE = new Main(); // singleton
+    private static final Main INSTANCE = new Main();
 
     public static void main(String... args) throws LoginException, InterruptedException {
         final var token = System.getenv("LILY_BOT_TOKEN");
