8fd01bd80111f92541297756427b99369c6cefa5
Sam Pritchard <sampritchard0801@gmail.com>
2021-05-14 03:38:43 +0100

    Remove ClassGraph dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ repositories {
 
 dependencies {
     implementation("net.dv8tion:JDA:4.2.1_262")
-    implementation("io.github.classgraph:classgraph:4.8.102")
     implementation("io.sentry:sentry-logback:5.0.0-beta.3")
 }
 
