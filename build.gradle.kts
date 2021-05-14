8f44d5157bad23651808e0f8f933df85bb0511fe
Sam Pritchard <sampritchard0801@gmail.com>
2021-05-14 03:24:40 +0100

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 
 repositories {
     jcenter()
-    maven(url = "https://jitpack.io")
+    maven(url = "https://m2.dv8tion.net/releases")
 }
 
 dependencies {
-    implementation("com.mewna:catnip:8a3ffef")
+    implementation("net.dv8tion:JDA:4.2.1_262")
     implementation("io.github.classgraph:classgraph:4.8.102")
 }
 
 application {
-    mainClass.set("io.github.samophis.lily.startup.Main")
+    mainClass.set("gg.samantha.lily.startup.Main")
 }
---
+++ src/main/java/gg/samantha/lily/startup/Main.java
@@ -0,0 +1,13 @@
+package gg.samantha.lily.startup;
+
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String... args) throws LoginException, InterruptedException {
+        final var token = System.getenv("LILY_BOT_TOKEN");
+        final var jda = JDABuilder.createDefault(token).build();
+        jda.awaitReady();
+    }
+}
