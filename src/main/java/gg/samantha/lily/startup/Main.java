355f9f36cb7d20f537cff29323a3acd986530851
Sam Pritchard <sampritchard0801@gmail.com>
2021-05-17 18:18:23 +0100

    Add Sentry DSN support

--- src/main/java/gg/samantha/lily/startup/Main.java
+++ src/main/java/gg/samantha/lily/startup/Main.java
@@ -1,11 +1,15 @@
 package gg.samantha.lily.startup;
 
 import gg.samantha.lily.commands.Manager;
+import io.sentry.Sentry;
 import net.dv8tion.jda.api.JDABuilder;
 import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String... args) throws LoginException, InterruptedException {
+        var sentryDsn = System.getenv("LILY_SENTRY_DSN");
+        sentryDsn = sentryDsn == null ? "" : sentryDsn;
+        Sentry.init(sentryDsn);
         final var token = System.getenv("LILY_BOT_TOKEN");
         final var jda = JDABuilder.createDefault(token).build();
         jda.addEventListener(new Manager(jda));
