05c021181045f472dda645eeb98ac4a1c1467af0
Sam Pritchard <sampritchard0801@gmail.com>
2021-08-31 23:47:00 +0100

    Return :ok if command is found.

--- lib/lily/consumer.ex
+++ lib/lily/consumer.ex
@@ -1,12 +1,23 @@
 defmodule Lily.Consumer do
   use Nostrum.Consumer
 
-  alias Nostrum.Api
+  alias Lily.Commands
 
   def start_link, do: Consumer.start_link(__MODULE__)
 
-  def handle_event({:MESSAGE_CREATE, msg = %{content: "l->ping"}, _ws_state}), do:
-    Api.create_message(msg.channel_id, "pong!")
+  def handle_event({:MESSAGE_CREATE, msg, _ws_state}) when msg.author.bot == false do
+    if String.starts_with?(msg.content, "lily!") do
+      content = String.slice(msg.content, 5..-1)
+      [head | tail] = String.split(content)
+
+      case Commands.fetch_command(head) do
+        nil -> :ignore
+        command ->
+          spawn(fn -> command.(msg, tail) end)
+          :ok
+      end
+    end
+  end
 
   def handle_event(_event), do: :noop
 end
