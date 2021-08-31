b264b74e7b3b0105f64697cce05fa1faabcd8fb2
Sam Pritchard <sampritchard0801@gmail.com>
2021-08-31 23:29:09 +0100

    update supervision tree

--- lib/lily.ex
+++ lib/lily.ex
@@ -2,7 +2,7 @@ defmodule Lily do
   use Application
 
   def start(_type, _args) do
-    children = [Lily.Consumer, {Lily.Commands, [%{}]}]
+    children = [Lily.Consumer, Lily.Commands]
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
