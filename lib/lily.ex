abcbf0783f2e0c6600edce7e2e19a62da2803dc3
Sam Pritchard <sampritchard0801@gmail.com>
2021-09-02 04:11:05 +0100

    Change child order in root supervision tree

--- lib/lily.ex
+++ lib/lily.ex
@@ -2,7 +2,7 @@ defmodule Lily do
   use Application
 
   def start(_type, _args) do
-    children = [Lily.Consumer, Lily.Commands]
+    children = [Lily.Commands, Lily.Consumer]
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
