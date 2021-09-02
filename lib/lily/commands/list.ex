52207b53e3312d9e52a78a29ba38203b7bdb4bb3
Sam Pritchard <sampritchard0801@gmail.com>
2021-09-02 04:51:22 +0100

    Add an actual child specification to list module

--- lib/lily/commands/list.ex
+++ lib/lily/commands/list.ex
@@ -2,10 +2,20 @@ defmodule Lily.Commands.List do
   import Lily.Commands.Server, only: [add_command: 2]
   import Nostrum.Api, only: [create_message: 2]
 
-  def start_link(init \\ :ok) do
-    add_command("pong", &ping/2)
+  defp ping(message, _args), do:
+    create_message(message.channel_id, "Pong!")
+
+  # Child spec stuff below
+
+  def child_spec(_init \\ :ok) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start, []}
+    }
   end
 
-  defp ping(message, args), do:
-    create_message(message.channel_id, "Pong!")
+  def start do
+    :ok = add_command("ping", &ping/2)
+    :ignore
+  end
 end
