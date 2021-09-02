8273db2af2b4270ac0bb4ad9681691b5050f7bd0
Sam Pritchard <sampritchard0801@gmail.com>
2021-09-02 01:51:01 +0100

    Provide default argument for commands start_link

--- lib/lily/commands.ex
+++ lib/lily/commands.ex
@@ -1,7 +1,8 @@
 defmodule Lily.Commands do
   use Agent
 
-  def start_link, do: Agent.start_link(fn -> %{} end, name: __MODULE__)
+  def start_link(_init \\ :ok), do:
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
 
   def fetch_command(name), do: Agent.get(__MODULE__, &Map.get(&1, name))
 
