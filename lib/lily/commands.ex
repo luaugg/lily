da79acdd8e42ffd656a3376208eacf68b742d5f4
Sam Pritchard <sampritchard0801@gmail.com>
2021-09-02 04:11:16 +0100

    Add a command macro

--- lib/lily/commands.ex
+++ lib/lily/commands.ex
@@ -7,4 +7,14 @@ defmodule Lily.Commands do
   def fetch_command(name), do: Agent.get(__MODULE__, &Map.get(&1, name))
 
   def add_command(name, function), do: Agent.update(__MODULE__, &Map.put(&1, name, function))
+
+  defmacro command(name, do: body) do
+    define = Macro.expand(name, __CALLER__)
+    fname = define |> elem(0) |> Atom.to_string
+
+    quote do
+      cmd_macro = def unquote(define), do: unquote(body)
+      Lily.Commands.add_command(unquote(fname), cmd_macro)
+    end
+  end
 end
